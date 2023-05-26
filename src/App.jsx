555dff933dcbd41ecb089a496627ea6d0b246685
Phil Nelson <nelsonpa@miamioh.edu>
2023-05-26 12:05:19 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import './App.css'
+import styled from 'styled-components'
+import Nav from './components/Nav'
+
+function App() {
+	return (
+		<>
+			<Wrapper>
+				<Nav></Nav>
+			</Wrapper>
+		</>
+	)
+}
+
+const Wrapper = styled.div`
+	position: absolute;
+	inset: 0;
+	height: 200vh;
+	background-color: hsla(
+		155.67567567567568,
+		86.04651162790702%,
+		96.5686274509804%,
+		0.85
+	);
+`
+
+export default App
---
+++ src/components/Nav.jsx
@@ -0,0 +1,76 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export default function Nav() {
+	return (
+        <Wrapper>
+		<NavWrapper>
+			<Spacer>
+				<Logo
+					src='src/assets/images/KanasonSucculentsLogoPng.png'
+					alt='kanason succulent logo'
+				/>
+			</Spacer>
+			<nav>
+				<LinkWrapper>
+					<LinkItem>
+						<Link href='#'>Home</Link>
+					</LinkItem>
+					<LinkItem>
+						<Link href='#'>Succulents</Link>
+					</LinkItem>
+					<LinkItem>
+						<Link href='#'>Dry&nbsp;Flowers</Link>
+					</LinkItem>
+				</LinkWrapper>
+			</nav>
+			<Spacer />
+		</NavWrapper>
+        </Wrapper>
+	)
+}
+
+const Wrapper = styled.div`
+    position: sticky;
+    top: -24px;
+    width: 100%;
+`;
+
+const NavWrapper = styled.header`
+    position: absolute;
+    top: 24px;
+	display: flex;
+	align-items: center;
+	height: 100px;
+    width: 100%;
+	background-color: hsl(147, 60%, 99%);
+	padding: 24px 12px;
+	filter: drop-shadow(0px 2px 12px hsl(0, 0%, 50%));
+`
+
+const Spacer = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+	flex: 1;
+	margin-inline: 24px;
+`
+
+const Logo = styled.img`
+    max-width: 124px;
+`
+
+const LinkWrapper = styled.ul`
+	display: flex;
+	gap: 24px;
+`
+
+const LinkItem = styled.li`
+	list-style-type: none;
+`
+
+const Link = styled.a`
+	text-decoration: none;
+	font-size: calc(24 / 16 * 1rem);
+	color: black;
+`
